b04d4beb63784e324cec7b52ab786edd40e8e9e0
22mahmoud <hello@mahmoudashraf.dev>
2021-02-19 17:46:38 +0200

    Parse multiple request in the file with query

--- lua/nvim_rest/init.lua
+++ lua/nvim_rest/init.lua
@@ -31,9 +31,17 @@ local function get_or_create_buf()
   return new_bufnr
 end
 
-local function get()
-  local line = vim.fn.getline(".")
-  local res = curl.get(line)
+local function get(opts)
+  local url = opts.url
+  local query = opts.query
+
+  local res =
+    curl.get(
+    {
+      url = url,
+      query = query
+    }
+  )
   local bufnr = get_or_create_buf()
 
   -- add the curl result into the created buffer
@@ -48,13 +56,46 @@ local function get()
   end
 end
 
+local function start()
+  local line = utils.split(vim.fn.getline("."), " ")
+  local method, url = line[1], line[2]
+
+  if method == "GET" then
+    local bufnr = api.nvim_win_get_buf(0)
+    local next_query = vim.fn.search("GET", "n", vim.fn.line("$"))
+    print(next_query)
+    local start_query_line_number =
+      vim.fn.search("{", "n", next_query > 1 and next_query or vim.fn.line("$"))
+    local end_query_line_number =
+      vim.fn.search("}", "n", next_query > 1 and next_query or vim.fn.line("$"))
+    local query = nil
+    if (start_query_line_number > 0) then
+      local query_string = ""
+      local query_lines =
+        api.nvim_buf_get_lines(
+        bufnr,
+        start_query_line_number - 1,
+        end_query_line_number,
+        false
+      )
+      for _, v in ipairs(query_lines) do
+        query_string = query_string .. v
+      end
+
+      query = vim.fn.json_decode(query_string)
+    end
+    get({url = url, query = query})
+  end
+end
+
 api.nvim_set_keymap(
   "n",
   ",w",
-  ":lua require('nvim_rest').get()<cr>",
+  ":lua require('nvim_rest').start()<cr>",
   {noremap = true, silent = true}
 )
 
 return {
-  get = get
+  get = get,
+  start = start
 }
--- lua/nvim_rest/utils.lua
+++ lua/nvim_rest/utils.lua
@@ -7,4 +7,15 @@ M.magiclines = function(s)
   return s:gmatch("(.-)\n")
 end
 
+M.split = function(inputstr, sep)
+  if sep == nil then
+    sep = "%s"
+  end
+  local t = {}
+  for str in string.gmatch(inputstr, "([^" .. sep .. "]+)") do
+    table.insert(t, str)
+  end
+  return t
+end
+
 return M
